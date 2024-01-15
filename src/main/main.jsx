c69fd9a6aaaba633ab59f0cef1744fda15fbba6a
Akhrorfayzullo <akhrorfayzullo@gmail.com>
2024-01-15 11:35:09 +0900

    2nd

--- src/main/main.jsx
+++ src/main/main.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import {
   BtnStore,
+  CardImg,
+  Cardh1,
   CenterDiv,
+  CommentDiv,
+  Commenth1,
   Genh1,
   MobileCard,
+  Simplediv,
   TitleH1,
 } from "../styled/styled";
 import apple from "../assets/image 2.png";
@@ -51,14 +56,139 @@ export default function Main() {
         </Genh1>
         <CenterDiv $btn={true}>
           <CenterDiv $cardOuter1={true}>
-            <MobileCard></MobileCard>
-            <MobileCard></MobileCard>
+            <MobileCard $small={true}>
+              <CardImg $small={true} />
+              <Simplediv>
+                <CardImg $small1in={true} />
+                <Cardh1>Create your family's emergency plan</Cardh1>
+                <Cardh1 $p={true}>
+                  Our app is tailored to you, your family and your little dog,
+                  too. We tell you what you need to stay safe from hurricanes to
+                  wildfires and everything in between.
+                </Cardh1>
+              </Simplediv>
+            </MobileCard>
+            <MobileCard $small2={true}>
+              <CardImg $small2={true} />
+              <Simplediv>
+                <CardImg $small2in={true} />
+                <Cardh1>
+                  Know exactly what to do when an emergency strikes
+                </Cardh1>
+                <Cardh1 $p={true}>
+                  When disasters happen, harbor automatically activates your
+                  readiness plan and sends you personalized, on-demand info when
+                  it matters the most.
+                </Cardh1>
+              </Simplediv>
+            </MobileCard>
           </CenterDiv>
+          {/* ************** */}
           <CenterDiv $cardOuter={true}>
-            <MobileCard></MobileCard>
-            <MobileCard></MobileCard>
+            <MobileCard $small3={true}>
+              <CardImg $small3={true} />
+              <Simplediv>
+                <CardImg $small3in={true} />
+                <Cardh1>
+                  Access and share important information, even offline
+                </Cardh1>
+                <Cardh1 $p={true}>
+                  View critical information, family plans, and documents at the
+                  push of a button, both online and offline - no worries if cell
+                  towers go out.
+                </Cardh1>
+              </Simplediv>
+            </MobileCard>
+
+            <MobileCard $small4={true}>
+              <CardImg $small4={true} />
+              <Simplediv>
+                <CardImg $small4in={true} />
+                <Cardh1>
+                  Get peace-of-mind with automatic risk assessments
+                </Cardh1>
+                <Cardh1 $p={true}>
+                  We use data from NOAA, FEMA, and USGS to pinpoint what your
+                  household is at risk for. No more guessing.
+                </Cardh1>
+              </Simplediv>
+            </MobileCard>
           </CenterDiv>
         </CenterDiv>
+      </CenterDiv>
+      <CenterDiv $we={true}>
+        <TitleH1 $we={true}>We're ready, are you?</TitleH1>
+        <Genh1 $we={true}>Here's what people are saying about harbor.</Genh1>
+      </CenterDiv>
+      <CenterDiv $comWrap={true}>
+        <CommentDiv>
+          <Commenth1>
+            It's beautiful, easy to use, and is genuinely growing my confidence
+            for crisis preparation.
+          </Commenth1>
+          <div>
+            <CardImg $star={true} />
+          </div>
+
+          <CenterDiv $com={true}>
+            <CardImg $appleGray={true} />
+            <div>
+              <Commenth1 $com={true}>Antonea</Commenth1>
+              <Commenth1>iOS Beta Tester</Commenth1>
+            </div>
+          </CenterDiv>
+        </CommentDiv>
+        <CommentDiv>
+          <Commenth1>
+            This app is something I’ve been hoping for for a while now. So I’m
+            excited to be beta testing it.
+          </Commenth1>
+          <div>
+            <CardImg $star={true} />
+          </div>
+
+          <CenterDiv $com={true}>
+            <CardImg $appleGray={true} />
+            <div>
+              <Commenth1 $com={true}>Kathryn</Commenth1>
+              <Commenth1>iOS Beta Tester</Commenth1>
+            </div>
+          </CenterDiv>
+        </CommentDiv>
+        <CommentDiv>
+          <Commenth1>
+            It’s a great app and I can’t wait to use it more and see what all
+            content you add.
+          </Commenth1>
+          <div>
+            <CardImg $star={true} />
+          </div>
+
+          <CenterDiv $com={true}>
+            <CardImg $appleGray={true} />
+            <div>
+              <Commenth1 $com={true}>Gabriel</Commenth1>
+              <Commenth1>iOS Beta Tester</Commenth1>
+            </div>
+          </CenterDiv>
+        </CommentDiv>
+      </CenterDiv>
+      <CenterDiv $thumb={true}>
+        <div>
+          <CardImg $thumb={true} />
+        </div>
+        <TitleH1 $thumb={true}>
+          Our mission is to
+          <br /> encourage readiness for all
+        </TitleH1>
+        <Genh1 $center={true} $thumb={true}>
+          It's our dream that one day, everyday readiness will be quite ordinary
+          - like brushing your teeth.
+        </Genh1>
+        <BtnStore $thumb={true}>
+          <Genh1 $thumb={true}>Learn more about us</Genh1>
+        </BtnStore>
+        <CardImg $thumbBack={true} />
       </CenterDiv>
     </div>
   );
--- src/styled/styled.js
+++ src/styled/styled.js
@@ -1,4 +1,16 @@
 import styled from "styled-components";
+import card1Img from "../assets/card1.png";
+import card2Img from "../assets/card2.png";
+import card3Img from "../assets/card3.png";
+import card4Img from "../assets/card4.png";
+import card1icon from "../assets/card1icon.png";
+import card2icon from "../assets/card2icon.png";
+import card3icon from "../assets/card3icon.png";
+import card4icon from "../assets/card4icon.png";
+import star from "../assets/star.png";
+import appleGray from "../assets/appleGray.png";
+import thumbsUp from "../assets/thumsUp.png";
+import thumbBackground from "../assets/thumbBackground.png";
 
 export const NavWrapper = styled.div`
   display: flex;
@@ -23,26 +35,49 @@ export const Smalldiv = styled.div`
   /* border: 2px solid gainsboro; */
 `;
 export const Genh1 = styled.h1`
-  color: #65727b;
+  color: ${(props) => (props.$thumb ? "#fff" : "#65727b")};
   text-align: center;
   font-family: Roboto;
   font-size: ${(props) => (props.$center ? "23px" : "16px")};
   font-style: normal;
-  margin: ${(props) => (props.$center ? "38px 0 50px" : "0px")};
+  margin: ${(props) => (props.$center ? "38px 0 50px" : "0")};
   font-weight: 400;
   line-height: 31px; /* 141% */
 `;
 export const CenterDiv = styled.div`
   display: flex;
-  flex-direction: ${(props) => (props.$btn ? "row" : "column")};
+  /* border: 10px solid olive; */
+  flex-direction: ${(props) =>
+    props.$btn || props.$com || props.$comWrap ? "row" : "column"};
   align-items: center;
-  justify-content: center;
-  margin: ${(props) => (props.$center ? "145px 0 0" : "0px")};
+  justify-content: ${(props) =>
+    props.$com ? "left" : props.$commWrap ? "space-around" : "center"};
+  margin: ${(props) =>
+    props.$center
+      ? "145px 0 0"
+      : props.$we
+      ? "340px 0px 100px"
+      : props.$cardOuter1
+      ? "230px 0 0"
+      : props.$thumb
+      ? "190px 0 50px"
+      : "0px"};
+
+  background-color: ${(props) => (props.$thumb ? "#28514F" : "none")};
+
   gap: ${(props) =>
-    props.$btn || props.$cardOuter1 || props.$cardOuter ? "20px" : "0"};
+    props.$btn ||
+    props.$cardOuter1 ||
+    props.$cardOuter ||
+    props.$com ||
+    props.$comWrap
+      ? "20px"
+      : props.$we
+      ? "36px"
+      : "0"};
 `;
 export const TitleH1 = styled.h1`
-  color: #28514f;
+  color: ${(props) => (props.$thumb ? "#fff" : "#28514f")};
   text-align: center;
   font-family: DM Serif Display;
   font-size: 48px;
@@ -51,18 +86,102 @@ export const TitleH1 = styled.h1`
   line-height: 55.68px; /* 116% */
 `;
 export const BtnStore = styled.button`
-  width: 156px;
+  width: ${(props) => (props.$thumb ? "204px" : "156px")};
   height: 56px;
   border: none;
   display: flex;
+  /* flex-direction: column-reverse; */
   align-items: center;
   justify-content: center;
   border-radius: 50px;
   background-color: #aeb4b6;
+  margin: ${(props) => (props.$thumb ? "0 0 50px" : "0")};
 `;
 export const MobileCard = styled.div`
+  display: flex;
+  flex-direction: ${(props) =>
+    props.$small || props.$small2 ? "column" : "column-reverse"};
+  align-items: center;
+  gap: 40px;
+  padding: 50px 60px;
   width: 460px;
-  height: 768px;
+  height: ${(props) => (props.$small || props.$small4 ? "768px" : "886px")};
   border-radius: 20px;
-  background-color: #dae1f4;
+  background-color: ${(props) => {
+    if (props.$small) {
+      return "#DAE1F4";
+    } else if (props.$small2) {
+      return `#E0F0E6`;
+    } else if (props.$small3) {
+      return `#D0E6F3`;
+    } else {
+      return "#F4F0E9"; // Default background image
+    }
+  }};
+  /* border: 1px solid red; */
+`;
+export const CardImg = styled.img.attrs((props) => ({
+  alt: "no img",
+  src: props.$small
+    ? card1Img
+    : props.$small2
+    ? card2Img
+    : props.$small3
+    ? card3Img
+    : props.$small4
+    ? card4Img
+    : props.$small1in
+    ? card1icon
+    : props.$small2in
+    ? card2icon
+    : props.$small3in
+    ? card3icon
+    : props.$small4in
+    ? card4icon
+    : props.$appleGray
+    ? appleGray
+    : props.$star
+    ? star
+    : props.$thumb
+    ? thumbsUp
+    : props.$thumbBack
+    ? thumbBackground
+    : undefined, // Default background image
+}))`
+  /* Other styles */
+  margin: ${(props) => (props.$thumb ? "-100px 0 170px" : "0")};
+`;
+export const Cardh1 = styled.h1`
+  color: #1b1f2b;
+  font-family: Roboto;
+  font-size: ${(props) => (props.$p ? "17px" : "40px")};
+  font-style: normal;
+  font-weight: ${(props) => (props.$p ? "400" : "700")};
+  line-height: ${(props) => (props.$p ? "23px" : "47px")}; /* 118% */
+`;
+export const Simplediv = styled.div`
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+`;
+export const CommentDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 294px;
+  height: 230px;
+  border-radius: 20px;
+  padding: 31px;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  background-color: #fff;
+`;
+export const Commenth1 = styled.h1`
+  color: #1b1f2b;
+  font-family: Merriweather;
+  font-size: ${(props) => (props.$com ? "16px" : "13px")};
+  font-style: normal;
+  font-weight: 400;
+  line-height: 20px; /* 153.846% */
 `;
